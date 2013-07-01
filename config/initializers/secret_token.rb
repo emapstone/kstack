6074bccfff334f51376a6d632174a77958cf0379
emapstone <ehardy@2025labs.com>
2013-07-01 13:34:42 -0700

    Default files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kidstack::Application.config.secret_token = '5c8c111bd8d8d9f5e1394dc15ecb27338f695bca5d4e0ac788acccf2d920c6113bbba53ffb1a422de669e394250a48ae2dd2f684982c0551a83281686eaf5256'
